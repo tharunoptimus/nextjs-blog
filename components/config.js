cb24269df8c609eb149aa647fcf938d7d132409f
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2022-01-17 14:23:22 +0530

    Removed blogs

--- components/config.js
+++ components/config.js
@@ -1,4 +1,4 @@
 export let authorName = "Tharunraj Optimus"
-export let siteTitle = "Next.js Sample Website"
+export let siteTitle = "That Blog"
 export let aboutAuthor = "Full Stack Web Developer"
 export let blogsAbout = "Blogs about various topics related to computers and stuff"
