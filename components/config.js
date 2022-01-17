51069f18b61a37f50026332a66565f816cce19ed
Tharunraj Optimus <59702671+tharunoptimus@users.noreply.github.com>
2022-01-17 14:15:45 +0530

    Added two posts

--- components/config.js
+++ components/config.js
@@ -1,4 +1,4 @@
 export let authorName = "Tharunraj Optimus"
 export let siteTitle = "Next.js Sample Website"
 export let aboutAuthor = "Full Stack Web Developer"
-export let blogsAbout = "Blogs about various topics related to Computers and stuff"
+export let blogsAbout = "Blogs about various topics related to computers and stuff"
